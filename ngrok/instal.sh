6fe377f25ce6cc304d00ff17d01dca2c015f85ab
Ruach Elohim <58249345+k3746ov@users.noreply.github.com>
2022-05-01 21:10:20 +0400

    Update instal.sh

--- ngrok/instal.sh
+++ ngrok/instal.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 echo
-echo
-echo
-echo
-echo
-echo
-echo
+print(
+""" install ngrok """)
 apt update && pkg update && apt upgrade && pkg upgrade
 apt install wget zip unzip
 echo
@@ -16,4 +12,4 @@ echo
 
 wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-arm.tgz
 
-tar -ztvf ngrok-v3-stable-linux-arm.tgz
+tar -xzvf ngrok-v3-stable-linux-arm.tgz
