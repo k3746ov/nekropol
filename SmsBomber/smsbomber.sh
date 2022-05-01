6a907bfbaca4373ae3ffb668ce0374af6a2c5460
Ruach Elohim <58249345+k3746ov@users.noreply.github.com>
2022-05-01 23:21:55 +0400

    Update smsbomber.sh

--- SmsBomber/smsbomber.sh
+++ SmsBomber/smsbomber.sh
@@ -1,1 +1,10 @@
 #!/usr/bin/bash
+echo
+aptupdate && apt upgrade -y
+clear
+load
+clear
+bom
+echo
+get_url=$(curl -s https://mytoolstown.com/smsbomber#bestsmsbomber)
+cek='curl -s '$get_url # check status
