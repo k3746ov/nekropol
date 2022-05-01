78edf3a447ea428d23d3b06464eb4587c9868c22
Ruach Elohim <58249345+k3746ov@users.noreply.github.com>
2022-05-01 20:36:22 +0400

    Update install.sh

--- ngrok/instal.sh
+++ ngrok/instal.sh
@@ -1,16 +1,50 @@
 #!/bin/bash
-echo
-echo
-echo
+#
+#
+#
+#
+#
+#
+#
+#selecting arch
+arch=$(dpkg --print-architecture)
+#selecting commands
+update="apt update -y >/dev/null"
+upgrade="apt upgrade -y >/dev/null"
+iwget="apt install wget zip unzip figlet -y"
+#simple condition sentence
+require(){
+	eval $update
+	eval $upgrade
+	eval $iwget
+}
 
-printf $g"Updating and Upgrading First..!\n"
-
-apt update -y;apt upgrade -y
-
-apt install  wget -y
-
-clear
-
-bash install.sh
-
-wget "https://github.com/k3746ov/nekropol/ngrok/files/master
+sleep 5
+if [ $arch == 'aarch64' ]
+then 
+echo -e "\033[92m[•] \033[93mInstalling the requirements... "
+require
+echo -e "\033[92m"
+figlet "ngrok"
+echo -e "\033[94m"
+figlet "for"
+echo -e "\033[95m"
+figlet $arch
+wget "https://bin.equinox.io/a/nmkK3DkqZEB/ngrok-2.2.8-linux-arm64.zip"
+unzip "ngrok-2.2.8-linux-arm64.zip"
+chmod 777 ngrok
+else 
+echo -e "\033[92m[•] \033[93mInstalling the requirements... "
+require
+echo -e "\033[92m"
+figlet "ngrok"
+echo -e "\033[94m"
+figlet "for"
+echo -e "\033[95m"
+figlet $arch
+wget https://github.com/k3746ov/nekropol/ngrok/raw/master/ngrok-arm
+mv ngrok-arm ngrok
+chmod 777 ngrok
+fi
+echo -e """\033[92m 
+ngrok has been installed successfully """
