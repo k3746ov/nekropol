876515093610552e334d7b8399072d73bf4dfd69
Ruach Elohim <58249345+k3746ov@users.noreply.github.com>
2022-05-01 21:14:03 +0400

    Update instal.sh

--- ngrok/instal.sh
+++ ngrok/instal.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 echo
-print(
-""" install ngrok """)
 apt update && pkg update && apt upgrade && pkg upgrade
-apt install wget zip unzip
+apt install wget zip unzip gunzip
 echo
-echo
-if [[ ! -a files/update.sh ]];then
-echo
-echo
-
+echo "Downloading Termux-ngrok..."
+#
 wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-arm.tgz
-
+#
 tar -xzvf ngrok-v3-stable-linux-arm.tgz
