4b18d08954d81ee416bdff2dd09e0e3de6a39662
Ruach Elohim <58249345+k3746ov@users.noreply.github.com>
2022-05-01 21:39:38 +0400

    Update instal.sh

--- ngrok/instal.sh
+++ ngrok/instal.sh
@@ -5,13 +5,16 @@ echo
 
 sleep 3.0
 
-echo "Downloading Termux-ngrok..."
+echo "Downloading Ngrok..."
 
 sleep 5.0
 #
 wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-arm.tgz
 #
 tar -xzvf ngrok-v3-stable-linux-arm.tgz
+
+sleep 1.0
+
 echo
 rm -rf ngrok-v3-stable-linux-arm.tgz
 
@@ -20,3 +23,8 @@ mv ngrok /$HOME
 cd $HOME
 
 chmod 777 ngrok
+
+echo "Installed ngrok and moved it to the home directory."
+echo " Link your account : "
+echo " ./ngrok config add-authtoken (You can find it from the authtoken account by opening an ngrok account.) "
+echo " Fire it up (./ngrok http 80) "
