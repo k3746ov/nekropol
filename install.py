53a46efea78f025d9b49970b412e6b6295bdb9a4
Ruach Elohim <58249345+k3746ov@users.noreply.github.com>
2022-05-16 17:45:43 +0400

    Update and rename install to install.py

---
+++ install.py
@@ -0,0 +1,11 @@
+import os, sys
+import readline
+from time import sleep as timeout
+from core.lzmcore import *
+
+def main():
+	print("   [01] Termux Packages")
+	print("   [02] Telegram Scraper")
+	print("   [03] Ngrok")
+	print("   [04] KaliNethunter")
+	print("   [05] Exit")
