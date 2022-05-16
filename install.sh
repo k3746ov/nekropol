dc840e1f46bacd65d5847e3c0e37f67a95210876
Ruach Elohim <58249345+k3746ov@users.noreply.github.com>
2022-05-16 19:27:03 +0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,16 +14,5 @@ echo
 
 read -p $' Choose an option:' option
 if [[ $option == 1 ]]; then
-cd nekropol/core
+cd $HOME/nekropol/core
 bash install-1.sh
-elif [[ $option == 2 || $option == 02 ]]; then
-cd $HOME/nekropol/core
-python3 adder-2.py
-elif [[ $option == 3 || $option == 03 ]]; then
-cd $HOME/nekropol/core
-bash install-3.sh
-elif [[ $option == 4 || $option == 04 ]]; then
-cd $HOME/nekropol/core
-bash install-4.sh
-elif [[ $option == 5 ]]; then
-exit
