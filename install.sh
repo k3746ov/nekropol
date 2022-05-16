b26f150a96d2f2aa61d3cff4af8d55c4ec20fc4e
Ruach Elohim <58249345+k3746ov@users.noreply.github.com>
2022-05-16 19:57:07 +0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,38 @@
 #!/bin/bash
 
 echo
-menu(){
-printf "\e[1;92m[\e[0m\e[1;77m1\e[0m\e[1;92m]\e[0m\e[1;93m Termux Packages\e[0m\n"
-printf "\e[1;92m[\e[0m\e[1;77m2\e[0m\e[1;92m]\e[0m\e[1;93m Telegram Scraper\e[0m\n"
-printf "\e[1;92m[\e[0m\e[1;77m3\e[0m\e[1;92m]\e[0m\e[1;93m Ngrok\e[0m\n"
-printf "\e[1;92m[\e[0m\e[1;77m4\e[0m\e[1;92m]\e[0m\e[1;93m KaliNethunter\e[0m\n"
-printf "\e[1;92m[\e[0m\e[1;77m5\e[0m\e[1;92m]\e[0m\e[1;93m Exit\e[0m\n"
-read -p $'\n\e[1;92m[\e[0m\e[1;77m*\e[0m\e[1;92m] Choose an option: \e[0m' option
-if [[ $option == 1 || $option == 01 ]]; then
+
+echo
+
+echo
+
+printf "
+[01]  Termux Packages
+[02]  Telegram Scraper
+[03]  Ngrok
+[04]  KaliNethunter
+[05]  Exit
+
+#user option
+
+read -e -p $'\33[33menter option\33[0m : ' option
+
+#option
+
+if [[ $option == 01 ]];then
 cd $HOME/nekropol/Core
 bash install-1.sh
-}
+elif [[ $option == 02 ]];then
+cd $HOME/nekropol/Core
+python3 adder-2.py
+elif [[ $option == 03 ]];then
+cd $HOME/nekropol/Core
+bash install-3.sh
+elif [[ $option == 04 ]];then
+cd $HOME/nekropol/Core
+bash install-4.sh
+elif [[ $option == 05 ]];then
+exit
+else
+printf " ERROR "
+fi
