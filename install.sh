e5d847cce021db7143d8e00a81ebb3caa9dc0981
Ruach Elohim <58249345+k3746ov@users.noreply.github.com>
2022-05-16 19:44:40 +0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
 
 echo
-
-printf "
-[01]  Termux Peckages
-[02]  Telegram Scraper
-[03]  Ngrok
-[04]  KaliNethunter
-[05]  Exit
-"
-
-echo
-
-read -e -p $' Choose an option:' option
-if [[ $option == 1 ]];then
+menu(){
+printf "\e[1;92m[\e[0m\e[1;77m1\e[0m\e[1;92m]\e[0m\e[1;93m Termux Packages\e[0m\n"
+printf "\e[1;92m[\e[0m\e[1;77m2\e[0m\e[1;92m]\e[0m\e[1;93m Telegram Scraper\e[0m\n"
+printf "\e[1;92m[\e[0m\e[1;77m3\e[0m\e[1;92m]\e[0m\e[1;93m Ngrok\e[0m\n"
+printf "\e[1;92m[\e[0m\e[1;77m4\e[0m\e[1;92m]\e[0m\e[1;93m KaliNethunter\e[0m\n"
+printf "\e[1;92m[\e[0m\e[1;77m5\e[0m\e[1;92m]\e[0m\e[1;93m Exit\e[0m\n"
+read -p $'\n\e[1;92m[\e[0m\e[1;77m*\e[0m\e[1;92m] Choose an option: \e[0m' option
+if [[ $option == 1 || $option == 01 ]]; then
 cd $HOME/nekropol/Core
 bash install-1.sh
+}
