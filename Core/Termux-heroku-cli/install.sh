8f4fdee6f28a707ef342e6c4dfd474967d3c114b
Ruach Elohim <58249345+k3746ov@users.noreply.github.com>
2022-08-14 21:00:06 +0400

    Update install.sh

--- Core/Termux-heroku-cli/install.sh
+++ Core/Termux-heroku-cli/install.sh
@@ -1,4 +1,13 @@
 #!/data/data/com.termux/files/usr/bin/bash
+clear
+printf " 
+\33[31m
+
+█░█ █▀▀ █▀█ █▀█ █▀▀ █░█ ▄▄ █▀▀ █░░ █
+█▀█ ██▄ █▀▄ █▄█ █▄▄ █▄█ ░░ █▄▄ █▄▄ █
+
+\33[0m
+
 pkg install wget tar gzip -y
 
 wget http://cli-assets.heroku.com/heroku-linux-arm.tar.gz -O heroku.tar.gz
