2fcb290dea4117dd623124c937541901c04de729
Ruach Elohim <58249345+k3746ov@users.noreply.github.com>
2022-08-29 06:25:34 +0400

    Update b_install.sh
    Signed-off-by: Ruach Elohim <58249345+k3746ov@users.noreply.github.com>

--- core/b_install.sh
+++ core/b_install.sh
@@ -8,10 +8,10 @@ clear
 
 echo
 
-cd $HOME && cd nekropol && cd core
+cd $HOME/nekropol/core
 
-cat "motd.txt" >> /data/data/com.termux/files/usr/etc/motd
+cat motd.txt >> /data/data/com.termux/files/usr/etc/motd
 
-cat "bash_bashrc.txt" >> /data/data/com.termux/files/usr/etc/bash.bashrc
+cat bash_bashrc.txt >> /data/data/com.termux/files/usr/etc/bash.bashrc
 
-$logout
+
